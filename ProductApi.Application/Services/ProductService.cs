724fe9048838a03d0708c1455714dacc78688c72
Pedro <pedrooliveiradevs@gmail.com>
2025-03-15 10:28:43 -0300

    Alterações

--- ProductApi.Application/Services/ProductService.cs
+++ ProductApi.Application/Services/ProductService.cs
@@ -12,9 +12,7 @@ namespace ProductApi.Application.Services
         private readonly IMapper _mapper;
         private readonly IMediator _mediator;
 
-        public ProductService(
-            IMapper mapper, 
-            IMediator mediator)
+        public ProductService(IMapper mapper, IMediator mediator)
         {
             _mapper = mapper;
             _mediator = mediator;
@@ -22,47 +20,96 @@ namespace ProductApi.Application.Services
 
         public async Task<IEnumerable<ProductDto>> GetAll()
         {
-            var products = await _mediator.Send(new GetAllProductsQuery());
-            return _mapper.Map<IEnumerable<ProductDto>>(products);
+            try
+            {
+                var products = await _mediator.Send(new GetAllProductsQuery());
+                return _mapper.Map<IEnumerable<ProductDto>>(products);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Erro ao buscar produtos", ex);
+            }
         }
 
         public async Task<ProductDto> GetById(int id)
         {
-            var product =  await _mediator.Send(new GetProductByIdQuery { Id = id});
-            return _mapper.Map<ProductDto>(product);
+            try
+            {
+                var product = await _mediator.Send(new GetProductByIdQuery { Id = id });
+                return _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Erro ao buscar produto com ID {id}", ex);
+            }
         }
 
         public async Task<IEnumerable<ProductDto>> GetAllByPage(int pageNumber, int pageSize)
         {
-            var products = await _mediator.Send(new GetProductsByPageQuery
+            try
             {
-                PageNumber = pageNumber,
-                PageSize = pageSize
-            });
+                var products = await _mediator.Send(new GetProductsByPageQuery
+                {
+                    PageNumber = pageNumber,
+                    PageSize = pageSize
+                });
 
-            return _mapper.Map<IEnumerable<ProductDto>>(products);
+                return _mapper.Map<IEnumerable<ProductDto>>(products);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Erro ao buscar produtos paginados", ex);
+            }
         }
 
         public async Task<IEnumerable<DashboardDto>> GetDashboard()
         {
-            return await _mediator.Send(new DashboardQuery());
+            try
+            {
+                return await _mediator.Send(new DashboardQuery());
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Erro ao buscar dados do dashboard", ex);
+            }
         }
 
         public async Task<int> Create(ProductDto productDto)
         {
-            var command = _mapper.Map<CreateProductCommand>(productDto);
-            return await _mediator.Send(command);
+            try
+            {
+                var command = _mapper.Map<CreateProductCommand>(productDto);
+                return await _mediator.Send(command);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Erro ao criar produto", ex);
+            }
         }
 
         public async Task Update(ProductDto productDto)
         {
-            var command = _mapper.Map<UpdateProductCommand>(productDto);
-            await _mediator.Send(command);
+            try
+            {
+                var command = _mapper.Map<UpdateProductCommand>(productDto);
+                await _mediator.Send(command);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Erro ao atualizar produto", ex);
+            }
         }
 
         public async Task Delete(int id)
         {
-            await _mediator.Send(new DeleteProductCommand { Id = id });
+            try
+            {
+                await _mediator.Send(new DeleteProductCommand { Id = id });
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Erro ao deletar produto com ID {id}", ex);
+            }
         }
     }
 }
--- ProductApi.WebAPI/Controllers/ProductController.cs
+++ ProductApi.WebAPI/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ProductApi.Application.Dtos;
 using ProductApi.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProductApi.WebAPI.Controllers
 {
@@ -20,96 +20,47 @@ namespace ProductApi.WebAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            try
-            {
-                return Ok(await _productService.GetAll());
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return Ok(await _productService.GetAll());
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            try
-            {
-                return Ok(await _productService.GetById(id));
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return Ok(await _productService.GetById(id));
         }
 
         [HttpGet("paged")]
         public async Task<IActionResult> GetAllByPage([FromQuery] int pageNumber, [FromQuery] int pageSize)
         {
-            try
-            {
-                var products = await _productService.GetAllByPage(pageNumber, pageSize);
-                return Ok(products);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var products = await _productService.GetAllByPage(pageNumber, pageSize);
+            return Ok(products);
         }
 
         [HttpGet("dashboard")]
         public async Task<IActionResult> GetDashboard()
         {
-            try
-            {
-                var result = await _productService.GetDashboard();
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var result = await _productService.GetDashboard();
+            return Ok(result);
         }
 
         [HttpPost]
         public async Task<IActionResult> Add([FromBody]ProductDto productDto)
         {
-            try
-            {
-                return Created((await _productService.Create(productDto)).ToString(), productDto);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return Created((await _productService.Create(productDto)).ToString(), productDto);
         }
 
         [HttpPut]
         public async Task<IActionResult> Update([FromBody] ProductDto productDto)
         {
-            try
-            {
-                await _productService.Update(productDto);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            await _productService.Update(productDto);
+            return NoContent();
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete([FromQuery] int id)
         {
-            try
-            {
-                await _productService.Delete(id);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            await _productService.Delete(id);
+            return NoContent();
         }
     }
 }
